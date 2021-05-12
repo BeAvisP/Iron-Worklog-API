2adf5ecc195c4162331fedb6342c2d420466c34c
Bea Avis <bea.avis.p@gmail.com>
2021-05-12 16:45:18 +0200

    Update user model

--- models/User.model.js
+++ models/User.model.js
@@ -4,6 +4,7 @@ const Schema = require('Schema');
 const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     password: { type: String, required: true },
     email: {
       type: String,
