2e5f03f5e603dc561811cee6451dfb96f2a60c51
Bea Avis <bea.avis.p@gmail.com>
2021-05-12 17:34:03 +0200

    Update auth routes with /logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -70,3 +70,8 @@ router.post('/login', (req, res, next) => {
     });
   })(req, res, next);
 });
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  return res.status(200).json({ message: 'Log out success!'});
+})
