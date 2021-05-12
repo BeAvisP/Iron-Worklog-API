2e2ba2e10d4cf5a97137ba84f56a2bf7054b6502
Sam Alafar <alafar.hesam@gmail.com>
2021-05-12 16:46:06 +0200

    created Journey model file

---
+++ models/Journey.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const journeySchema = new Schema(
+  {
+    startHour: { type: Date, required: true },
+    endHour: { type: Date },
+    startBreak: { type: Date },
+    endBreak: { type: Date },
+    morningStandup: { type: String },
+    eveningStandup: { type: String },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+const Journey = mongoose.model('Journey', journeySchema);
+module.exports = Journey;
