3aae7ced98adf145bbc49856621b1634851be256
Sam Alafar <alafar.hesam@gmail.com>
2021-05-19 14:44:36 +0200

    updated cors file

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,10 +1,18 @@
 const cors = require('cors');
+const whitelist = [process.env.PUBLIC_DOMAIN, process.env.PUBLIC_DOMAIN_SECURE]
 
 module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: [process.env.PUBLIC_DOMAIN, process.env.PUBLIC_DOMAIN_SECURE],
+      origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
     })
   );
 };
+
